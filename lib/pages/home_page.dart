67557429401a51801918b402966323e6eb2ff8f3
irv-me <tareastareas89@gmail.com>
2025-02-07 13:33:59 -0600

    First commit

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold (
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        //Icono izquierdo
+        leading: Icon(
+          Icons.menu,
+          color: Colors.grey[800]
+          ),
+          //icono Derecho
+          actions: [
+            Icon(Icons.person)
+          ],
+      )
+    );
+  }
+}
