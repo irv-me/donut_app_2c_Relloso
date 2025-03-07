af182200babb2c1c293799a64ce873abf4a5f23c
irv-me <tareastareas89@gmail.com>
2025-03-07 13:22:06 -0600

    Donut Tile finished

--- lib/utils/donut_tile.dart
+++ lib/utils/donut_tile.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class DonutTile extends StatelessWidget {
   final String donutFlavor;
+  final String donutShop;
   final String donutPrice;
   //Es dynamic porque sera de tipo COLORS
   final dynamic donutColor;
@@ -10,6 +11,7 @@ class DonutTile extends StatelessWidget {
   const DonutTile(
       {super.key,
       required this.donutFlavor,
+      required this.donutShop,
       required this.donutPrice,
       this.donutColor,
       required this.imageName});
@@ -32,33 +34,61 @@ class DonutTile extends StatelessWidget {
                     decoration: BoxDecoration(
                         color: donutColor[100],
                         borderRadius: BorderRadius.only(
-                          bottomLeft: Radius.circular(24),
-                          bottomRight: Radius.circular(24)
-                        )),
+                            bottomLeft: Radius.circular(24),
+                            topRight: Radius.circular(24))),
                     padding:
                         const EdgeInsets.symmetric(vertical: 8, horizontal: 18),
                     child: Text("\$$donutPrice",
                         style: TextStyle(
                             fontSize: 18,
-                             fontWeight: FontWeight.bold,
-                             color: donutColor[800])
-                             ))
+                            fontWeight: FontWeight.bold,
+                            color: donutColor[800])))
               ],
             ),
 
             //donut picture
             Padding(
-              padding: const EdgeInsets.symmetric(
-                  vertical: 12,
-                  horizontal: 24),
+              padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 24),
               child: Image.asset(imageName),
-            )
+            ),
 
             //donut tag
 
             //donut flavor text
+            Text(donutFlavor,
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+            //Espacio entre textos
+            const SizedBox(
+              height: 4,
+            ),
+
+            Text(donutShop,
+                style: TextStyle(
+                    fontWeight: FontWeight.w100,
+                    fontSize: 15,
+                    color: Colors.grey[800])),
+            //Espacio entre textos
+            const SizedBox(
+              height: 4,
+            ),
 
             //Buttons
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 15),
+                  child: Icon(
+                    Icons.favorite_border_outlined,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 15),
+                  child: Text("Add",style: TextStyle(fontWeight: FontWeight.w900,fontSize: 20, decoration:TextDecoration.underline),),
+                )
+                
+              ],
+            ),
           ],
         ),
       ),
